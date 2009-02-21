0f4193770acd81ca3279cb57a0a5b168e4d8cf46
crazy2be <crazy1be+github@gmail.com>
2009-02-21 04:46:59 +0000

    adding tutorial source
    git-svn-id: http://code.google.com/archive/p/litestepportable/source/trunk@117 38ca8cbe-b76d-4e91-b741-e71bb1f618b7

---
+++ Other/Source/Tutorial/main.cpp
@@ -0,0 +1,34 @@
+#include <windows.h>
+#include "HTMLWindow.h"
+
+int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE unused__, LPSTR lpszCmdLine, int nCmdShow) {
+
+  HTMLWindow* html_window = new HTMLWindow (
+
+      "<html><head>"
+      "<title>MSHTMLTest</title>"   // seems a little useless in this context
+      "</head><body>"
+      "<h1>This is a test</h1>"
+      "I offer the following links:"
+      "<ul>"
+      "<li><a href='http://www.google.com'>www.google.com</a>"
+      "<li><a href='http://www.adp-gmbh.ch'>www.adp-gmbh.ch</a>"
+      "<li><a href='http://www.yahoo.com'>www.yahoo.com</a>"
+      "</ul>"
+      "</body></html>",
+       "MSHTMLTest", hInstance,
+      false  // not an url 
+       );
+
+  MSG msg;
+  while (GetMessage(&msg, 0, 0, 0)) {
+    TranslateMessage(&msg);
+    if (msg.message >= WM_KEYFIRST && 
+        msg.message <= WM_KEYLAST) {
+      ::SendMessage(html_window->hwnd_, msg.message, msg.wParam, msg.lParam);
+    }
+    DispatchMessage(&msg);
+  }
+
+  return 0;
+}
